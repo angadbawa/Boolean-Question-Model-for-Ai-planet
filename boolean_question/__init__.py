cb2b76a85edf7bd3fc8e0cd7bbe48b60d8d0268b
angadbawa <86665541+angadbawa@users.noreply.github.com>
2023-03-15 17:02:14 +0530

    Update __init__.py

--- boolean_question/__init__.py
+++ boolean_question/__init__.py
@@ -6,6 +6,4 @@ python pytorch model. The model takes a passage and question as
 input and returns either "True" or "False" as predicted answer.
 """
 
-__author__ = "Angad"
-
 from boolean_question.boolq import BoolQ
